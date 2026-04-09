92d2109ee91de1a7b8be437fd5e4d41122bc027f
Ruimeng Yang <ry2468@nyu.edu>
2026-04-09 15:53:33 -0400

    Add Active Storage config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rottenpotatoes::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  config.active_storage.service = :local
 
   # Compress JavaScripts and CSS
   # config.assets.js_compressor = :uglifier
